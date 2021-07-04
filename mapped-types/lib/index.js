7d336ac6c40561808f36e1049efdbfe5881627fc
afaf-tech <fatihalfikri5@gmail.com>
2021-07-04 10:35:19 +0700

    mapped-type modifiers

--- mapped-types/lib/index.js
+++ mapped-types/lib/index.js
@@ -1,6 +1,17 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.origin = void 0;
+exports.State = exports.origin = void 0;
 // export type ReadOnly<T> = {
 //    readonly [Key in keyof T]: T[Key]
 // };
@@ -9,5 +20,19 @@ exports.origin = {
     y: 0,
 };
 console.log(exports.origin);
-// origin.x = 2; // Should Error
+var point = {};
+var State = /** @class */ (function () {
+    function State(current) {
+        this.current = current;
+    }
+    State.prototype.update = function (next) {
+        this.current = __assign(__assign({}, this.current), next);
+    };
+    return State;
+}());
+exports.State = State;
+var state = new State({ x: 3, y: 4 });
+state.update({ x: 43 });
+state.update({ y: 4329487234 });
+console.log('state ', state.current);
 //# sourceMappingURL=index.js.map
--- mapped-types/src/index.ts
+++ mapped-types/src/index.ts
@@ -16,3 +16,33 @@ export const origin: Readonly<Point> ={
 console.log(origin);
 
 // origin.x = 2; // Should Error
+
+
+// Mapped advance Type
+export type Coordinate ={
+    readonly x: number,
+    y?: number,
+}
+
+export type Mapped<T> = {
+    -readonly [P in keyof T]+?: T[P] // - in modifier to remove from lookup type and  + is the opposite
+}
+
+export type Result= Mapped<Coordinate>;  // become x & y is optional and not readonly
+const point: Result = {
+}
+
+
+export class State<T> {
+    constructor (public current: T){}
+    update(next: Partial<T>){ // partial 
+        this.current = {...this.current, ...next};
+    }
+}
+
+
+const state = new State({x: 3, y: 4});
+state.update({x: 43})
+state.update({y: 4329487234})
+
+console.log('state ',state.current);
